9c8acacd7e8ec12cd86a54f60a935a605c4a86ed
on22vy <thi.nguyen.22@lehre.mosbach.dhbw.de>
2023-05-03 13:46:33 +0200

    Update index.ts - Add event listeners and initialize app functionality

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-//THIS IS THE ENTRY FILE - WRITE YOUR MAIN LOGIC HERE!
+import { addBtn, taskInput } from "./dom-utils";
+import { addTask, renderTasklist } from "./tasklist";
+
+
+
+function initApp(){
+    renderTasklist();
+
+    addBtn.addEventListener('click',()=>{
+        addTask();
+        renderTasklist();
+        taskInput.value = '';
+    });
+    
+    taskInput.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            addTask();
+            renderTasklist();
+            taskInput.value = '';
+        }
+    });
+}
+   
+initApp();

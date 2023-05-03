61548456a06b59ea0e20d58e3e593081b812a9a2
on22vy <thi.nguyen.22@lehre.mosbach.dhbw.de>
2023-05-03 13:35:31 +0200

    Add dom-utils.ts - Add DOM querySelector Method to retrieve task-list, task-input, and add-btn elements - Export all variables to reuse

---
+++ src/dom-utils.ts
@@ -0,0 +1,5 @@
+const taskList = document.querySelector('.task-list') as HTMLUListElement;
+const taskInput = document.querySelector('.task-input') as HTMLInputElement;
+const addBtn = document.querySelector('.add-btn') as HTMLButtonElement;
+
+export{taskList, taskInput, addBtn}

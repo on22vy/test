2b993ead961f6d4bbd18ef56e8307c8843484d28
on22vy <thi.nguyen.22@lehre.mosbach.dhbw.de>
2023-05-04 02:22:11 +0200

    Update tasklist.ts - deleteTask: delete a task from task list when the trash icon is clicked -markCompletedTask: mark tasks as completed when the checkmark icon is clicked

--- src/tasklist.ts
+++ src/tasklist.ts
@@ -2,18 +2,25 @@ import { taskInput, taskList } from "./dom-utils";
 
 const tasks : Array<Tasks> = [];
 
+//Add new task input to task list
 function addTask() {
     const beschreibung = taskInput.value;
     if (beschreibung) {
         tasks.push({
             beschreibung,
+            completed: false
         });
     }
+
+    renderTasklist();
+    
 }
 
-function renderTasklist() {
-    taskList.innerHTML = '';
 
+function renderTasklist() {
+
+    //Render the list of tasks using the styles defined in the CSS files.
+    taskList.innerHTML = '';
     tasks.forEach(task => {
 
         const taskContainer = document.createElement('div');
@@ -37,7 +44,37 @@ function renderTasklist() {
         taskContainer.appendChild(trashIcon);
 
         taskList.appendChild(taskContainer);
+
+        //delete Task when the trask icon is clicked
+        trashIcon.addEventListener('click', ()=>{
+            deleteTask(task, taskContainer);// call the deleteTask function
+        })
+
+        //Ensuring that the CSS "completed-task" class is added to the task container and the "cross-out" class is added to the list item element when the task is initially rendered, if the task's "completed" property is true.
+        //It means when adding new task, the completed tasks in the list will be still crossed out
+        if (task.completed) {
+            markCompletedTask(task, liEl, taskContainer);
+        }
+
+        //The CSS "completed-task" class is added to the task container and the "cross-out" class is added to the list item element when the checkmark icon is clicked
+        checkIcon.addEventListener('click', ()=> {
+            task.completed = !task.completed;
+            markCompletedTask(task, liEl, taskContainer);
+        })
     });
+
+}
+
+//Delete task from the list
+function deleteTask(task: Tasks, taskContainer: HTMLDivElement) {
+    taskList.removeChild(taskContainer);//delete task
+    const index = tasks.indexOf(task);
+    tasks.splice(index, 1);// remove deleted task from the tasks array
+}
+//Marking task as completed
+function markCompletedTask(task: Tasks, liEl: HTMLLIElement, taskContainer: HTMLDivElement) { 
+    taskContainer.classList.toggle("completed-task");
+    liEl.classList.toggle("cross-out");
 }
 
 export {addTask, renderTasklist}

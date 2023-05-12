cb7d408a10277d1dfaad636c5ec2cea3ade4749b
on22vy <thi.nguyen.22@lehre.mosbach.dhbw.de>
2023-05-12 03:05:37 +0200

    Update dom-utils.ts - add DOM element selectors to target related HTML elements which are used for pomodoro timer

--- src/dom-utils.ts
+++ src/dom-utils.ts
@@ -1,5 +1,15 @@
 const taskList = document.querySelector('.task-list') as HTMLUListElement;
 const taskInput = document.querySelector('.task-input') as HTMLInputElement;
-const addBtn = document.querySelector('.add-btn') as HTMLButtonElement;
+const addButton = document.querySelector('.add-btn') as HTMLImageElement;
+const modeButtons = document.querySelector('#js-mode-buttons') as HTMLDivElement;
+const timerContainer = document.querySelector('.timer') as HTMLDivElement;
+const minutesElement = document.getElementById('js-minutes') as HTMLElement;
+const secondsElement = document.getElementById('js-seconds') as HTMLElement;
+const startButton = document.querySelector('.start-button') as HTMLButtonElement;
+const resetButton = document.querySelector('.reset-btn') as HTMLImageElement;
+const buttonSound = document.querySelector('#start-button-sound') as HTMLAudioElement;
+const breakSound = document.querySelector("#break-sound") as HTMLAudioElement;
+const backToWorkSound = document.querySelector("#back-to-work-sound") as HTMLAudioElement;
+const tomatoIconsContainer = document.querySelector('.tomato-icons-container') as HTMLDivElement;
 
-export{taskList, taskInput, addBtn}
+export{taskList, taskInput, modeButtons, timerContainer, addButton, startButton, resetButton, buttonSound, breakSound, backToWorkSound, minutesElement, secondsElement, tomatoIconsContainer }

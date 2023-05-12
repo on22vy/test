56c8ef1c6317d0cf4a4b0081e6fab4ff9cf1f734
on22vy <thi.nguyen.22@lehre.mosbach.dhbw.de>
2023-05-12 05:15:59 +0200

    Update pomodoro.ts - Make code cleaner and more understandable - Rewrite function switchMode -> switchModeTo - Add function switchModeOnClick - move variables activeButton and activeMode into function getActiveMode to make them more reusable - simplify updateTimerDisplay -> updateClock - add functionality to track the completed pomodoros - add function to switch mode automatically at the end of each timer - add function to ask for permission to show notification - add function to show notification at the end of each mode

--- src/pomodoro.ts
+++ src/pomodoro.ts
@@ -1,122 +1,238 @@
-import { backToWorkSound, breakSound, buttonSound, startButton, timerContainer } from "./dom-utils";
+import { backToWorkSound, breakSound, buttonSound, minutesElement, secondsElement, startButton, timerContainer, tomatoIconsContainer } from "./dom-utils";
 
+
+
+// Initialize the timer object with initial values
 const timer: Timer = {
-    pomodoro: 25,
-    shortBreak: 5,
-    longBreak: 15,
+    pomodoro: 1,
+    shortBreak: 1,
+    longBreak: 1,
+    isPaused: true
 };
 
-const min = document.getElementById('js-minutes') as HTMLElement;
-const sec = document.getElementById('js-seconds') as HTMLElement;
-let interval: ReturnType<typeof setTimeout>;
-let paused = true;
 
-function switchMode(event: Event) {
-    const currentMode = (event.target as HTMLButtonElement).dataset.mode;
-    if (!currentMode) {
-      return;
+
+// Perform actions when the DOM has finished loading
+document.addEventListener('DOMContentLoaded', () => {
+    // Let's check if the browser supports notifications
+    if ('Notification' in window) {
+      // If notification permissions have neither been granted or denied
+      if (Notification.permission !== 'granted' && Notification.permission !== 'denied') {
+        // ask the user for permission
+        Notification.requestPermission().then(function(permission) {
+          // If permission is granted
+          if (permission === 'granted') {
+            // Create a new notification
+            new Notification(
+              'Awesome! You will be notified at the start of each session'
+            );
+          }
+        });
+      }
+    }
+    // Switch the mode to 'pomodoro' initially
+    switchModeTo('pomodoro');
+});
+
+
+
+// Function to switch the timer mode
+function switchModeTo(mode: string) {
+    const buttons = document.querySelectorAll('button[data-mode]');
+
+    // Iterate through the buttons and update their active states
+    buttons.forEach(button => {
+        const targetButton = button as HTMLButtonElement; 
+        if (targetButton.dataset.mode === mode) {
+            targetButton.classList.add('active');
+        } else {
+            targetButton.classList.remove('active');
+        }
+    });
+    
+    // Switch background color based on the mode
+    timerContainer.style.backgroundColor = `var(--${mode})`;
+
+    // Update the Clock UI
+    updateClock(mode);
+    
+}
+
+
+
+// Function to handle the click event on mode buttons
+function switchModeOnClick(event: Event) {
+    const clickedButton = event.target as HTMLButtonElement;
+
+    // Do nothing if the clicked element is not a mode button
+    if (!clickedButton.classList.contains('mode-button')) {
+        return; 
     }
 
+    // Get the clicked mode and switch to that mode
+    const clickedMode = `${clickedButton.dataset.mode}`;
+    switchModeTo(clickedMode);
+
+    // Reset the timer
     resetTimer();
-    //Switch button style
-    const activeButton = document.querySelector(`button[data-mode="${currentMode}"]`) as HTMLButtonElement;
-    const buttons = document.querySelectorAll('button[data-mode]');
-    buttons.forEach(button => {
-      if (button === activeButton) {
-        button.classList.add('active');
-      } else {
-        button.classList.remove('active');
-      }
-    });
-
-     //Switch background color
-    timerContainer.style.backgroundColor = `var(--${currentMode})`;
-
-    //Update Clock UI
-    updateTimerDisplay(currentMode);
-
 }
 
 
-
-function resetTimer(){
-    clearTimeout(interval);
+// Function to retrieve the active mode
+function getActiveMode() {
     const activeButton = document.querySelector('button.active') as HTMLButtonElement;
     const activeMode = `${activeButton.dataset.mode}`;
-    
+    return activeMode;
+}
+
+
+// Function to reset the timer
+function resetTimer(){
+    clearTimeout(countdownInterval);
+    const activeMode = getActiveMode();
+
     //Update Clock UI
-    updateTimerDisplay(activeMode);
-    paused = true;
+    updateClock(activeMode);
+
+    // Set the timer to paused state
+    timer.isPaused = true;
     
 }
 
 
-
-function updateTimerDisplay(mode: string) {
+// Function to update the clock UI
+function updateClock(mode: string) {
     startButton.innerHTML = "Start";
-    if(mode === "pomodoro"){
-        if (min !== null) {
-            min.innerHTML = `${timer.pomodoro}`.padStart(2, '0');
-        }
-    }else if (mode === "shortBreak"){
-        if (min !== null) {
-            min.innerHTML = `${timer.shortBreak}`.padStart(2, '0');
-        }
-    }else if (mode === "longBreak"){
-        if (min !== null) {
-            min.innerHTML = `${timer.longBreak}`.padStart(2, '0');
-        }
-    } 
-    sec.innerHTML = "00";
+    if (minutesElement !== null) {
+        minutesElement.innerHTML = `${timer[mode]}`.padStart(2, '0');
+    }
+    secondsElement.innerHTML = "00";
 }
 
 
-  
+// Variable to hold the countdown interval
+let countdownInterval: ReturnType<typeof setTimeout>;
+
+// Variable to keep track of completed pomodoros
+let completedPomodoros = 0;
+
+// Function to start the countdown
 function startCountdown() {
-    clearTimeout(interval);
-    if(paused) {
-        paused = false;
+    clearTimeout(countdownInterval);
+    if(timer.isPaused) {
+        // If the timer is paused, start/resume it
+        timer.isPaused = false;
         startButton.innerHTML = "Pause";
-        let minutes = parseInt(min.innerHTML);
-        let seconds = parseInt(sec.innerHTML);
+        let minutes = parseInt(minutesElement.innerHTML);
+        let seconds = parseInt(secondsElement.innerHTML);
 
         let endTime = new Date();
         endTime.setMinutes(endTime.getMinutes() + minutes); 
         endTime.setSeconds(endTime.getSeconds() + seconds);
 
+        // Function to perform the countdown
         function countdown() {
             let now = new Date();
             let remainingTime = endTime.getTime() - now.getTime(); //in Miliseconds
             if (remainingTime <= 0) {
-                const activeButton = document.querySelector('button.active') as HTMLButtonElement;
-                const activeMode = `${activeButton.dataset.mode}`;
-                playEndTimerSound(activeMode);
                 // Handle when timer ends
-                min.innerHTML = '00';
-                sec.innerHTML = '00';
-                resetTimer();
+
+                // Get the active mode
+                const activeMode = getActiveMode();
+
+                //Play the sound when the timer ends based on the active mode
+                playEndTimerSound(activeMode);
+                if (activeMode === 'pomodoro') {
+                    // Increment the count of completed pomodoros
+                    completedPomodoros++;
+                    // Create a new tomato icon to track the completed pomodoro
+                    const tomatoIcon = document.createElement('img');
+                    tomatoIcon.src = '/images/tomato-icon.png';
+                    tomatoIcon.classList.add('tomato-icon');
+                    tomatoIconsContainer.appendChild(tomatoIcon);
+ 
+                    if (completedPomodoros === 4) {
+                        // Switch to long break mode after completing 4 pomodoros
+                        switchModeTo('longBreak'); 
+                        showNotification('longBreak');
+                        resetTimer();
+                        startCountdown();
+                            
+                    } else {
+                        // Switch to short break mode
+                        switchModeTo('shortBreak');
+                        showNotification('shortBreak')
+                        resetTimer();
+                        startCountdown();
+                    }
+                    
+                } else if(activeMode === 'longBreak') {
+                    // Refresh tomato icons container and switch back to pomodoro mode
+                    tomatoIconsContainer.innerHTML = '';
+                    switchModeTo('pomodoro');
+                    showNotification('pomodoro');
+                    resetTimer();
+                    
+                }else{
+                    // Switch back to pomodoro mode
+                    switchModeTo('pomodoro');
+                    showNotification('pomodoro');
+                    resetTimer();
+                    startCountdown();
+                }
 
             } else {
-                let remainingSeconds = Math.round(remainingTime / 1000);
+                // Handle while timer is running
+
+                // Calculate remaining time
+                let remainingSeconds = Math.ceil(remainingTime / 1000);
                 minutes = Math.floor(remainingSeconds / 60);
                 seconds = remainingSeconds % 60;
-                if (min !== null) {
-                    min.innerHTML = `${minutes}`.padStart(2, '0');
+
+                // Update the minutes and seconds on the UI
+                if (minutesElement !== null) {
+                    minutesElement.innerHTML = `${minutes}`.padStart(2, '0');
                 }
-                if (sec !== null) {
-                    sec.innerHTML = `${seconds}`.padStart(2, '0');
+                if (secondsElement !== null) {
+                    secondsElement.innerHTML = `${seconds}`.padStart(2, '0');
                 }
-                interval = setTimeout(countdown, 1000);
+                // Continue the countdown
+                countdownInterval = setTimeout(countdown, 1000);
             }
         }
-        interval = setTimeout(countdown, 1000);
+        // Start the initial countdown
+        countdown();
+
     }else {
+        // If the timer is running, pause it
+        timer.isPaused = true;
         startButton.innerHTML = "Resume";
-        paused = true;
-        clearTimeout(interval);
+        clearTimeout(countdownInterval);
     }
 }
 
+
+// Function to show notification based on the mode
+function showNotification(mode: string) {
+    if (Notification.permission === 'granted') {
+        let notificationText = '';
+        if (mode === 'pomodoro') {
+            notificationText = 'Get back to work!';
+        } else if (mode === 'shortBreak'){
+            notificationText = 'Take a break!';
+        } else if (mode === 'longBreak') {
+            notificationText = 'Congratulations!';
+        }
+
+        if (notificationText !== '') {
+            new Notification(notificationText);
+        }
+    }
+}
+
+
+
+//Function to play sound effect based on the mode
 function playEndTimerSound(mode:String){
     if (mode === "pomodoro") {
     breakSound.play();
@@ -127,8 +243,12 @@ function playEndTimerSound(mode:String){
     }
 }
 
+
+
+//Function to play start button sound effect
 function playStartButtonSound(){
      buttonSound.play();
 }
 
-export{startCountdown, switchMode, resetTimer, playStartButtonSound, playEndTimerSound}
+
+export{startCountdown, switchModeOnClick, resetTimer, playStartButtonSound, playEndTimerSound}
